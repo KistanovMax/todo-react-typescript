0ffce755741954e11620dd9c36bbaba303927f6c
Maxim Kistanov <maxim.kistanov@gmail.com>
2021-01-24 16:45:37 +0300

    refactored

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,9 +1,12 @@
+export const ADD_TODO = "ADD_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+
 export const addTodo = (id, text, date, time, important) => ({
-  type: "ADD_TODO",
+  type: ADD_TODO,
   payload: { id, text, date, time, important },
 });
 
 export const deleteTodo = (id) => ({
-  type: "DELETE_TODO",
+  type: DELETE_TODO,
   payload: id,
 });
--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,12 +1,13 @@
-import { TODOS } from "../../api/data";
+import data from "../../api/data.json";
+import { ADD_TODO, DELETE_TODO } from "../actions/actions";
 
-const dataReducer = (state = TODOS, action) => {
+const dataReducer = (state = data, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       return (state = [...state, { ...payload }]);
-    case "DELETE_TODO":
+    case DELETE_TODO:
       return (state = [...state.filter((todo) => todo.id !== payload)]);
     default:
       return state;
